c4634bb1ac8e2addd767b70ce45d0660b30b3496
fpineda-2021274 <fpineda-2021274@kinal.edu.gt>
2026-02-15 13:04:58 -0600

    fix: modificando y agregando mi ruta de js

--- ProyectoBancario3/configs/app.js
+++ ProyectoBancario3/configs/app.js
@@ -18,6 +18,7 @@ import {
 import authRoutes from '../src/auth/auth.routes.js';
 import userRoutes from '../src/users/user.routes.js';
 import userRoute from '../src/Clientes/clientes.routes.js';
+import cuentasRoutes from '../src/Cuenta/cuentas.routes.js';
 import transactionRoutes from '../src/Transacciones/transaction.routes.js';
 const BASE_PATH = '/api/v1';
 
@@ -35,6 +36,7 @@ const routes = (app) => {
   app.use(`${BASE_PATH}/users`, userRoutes);
   app.use(`${BASE_PATH}/clientes`, userRoute);
   app.use(`${BASE_PATH}/transactions`, transactionRoutes);
+  app.use(`${BASE_PATH}/Cuenta`, cuentasRoutes);
 
   app.get(`${BASE_PATH}/health`, (req, res) => {
     res.status(200).json({
