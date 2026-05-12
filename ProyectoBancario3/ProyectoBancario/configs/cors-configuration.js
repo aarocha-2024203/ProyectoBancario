e83be941cd1d831060ec48554028d564db24f6f7
jregil-2021564 <jregil-2021564@kinal.edu.gt>
2026-05-11 23:06:56 -0600

    fix: Modificando el archivo cors configuration y se agregaron una nueva seccion de allowedHeaders

--- ProyectoBancario3/ProyectoBancario/configs/cors-configuration.js
+++ ProyectoBancario3/ProyectoBancario/configs/cors-configuration.js
@@ -2,5 +2,5 @@ export const corsOptions = {
     origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma'],
 };
