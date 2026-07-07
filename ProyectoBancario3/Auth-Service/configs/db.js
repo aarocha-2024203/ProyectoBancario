f775e9dfc097c174395e8460bc7606e5158ce222
jregil-2021564 <jregil-2021564@kinal.edu.gt>
2026-07-06 20:30:38 -0600

    fix: quitar override de dotenv y actualizar MONGO_URI

--- ProyectoBancario3/Auth-Service/configs/db.js
+++ ProyectoBancario3/Auth-Service/configs/db.js
@@ -13,24 +13,17 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 export const sequelize = new Sequelize({
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     logging: process.env.DB_SQL_LOGGING === 'true' ? console.log : false,
-    define: {
-    freezeTableName: true, // Usar nombres exactos sin pluralización
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-    underscored: true, // Usar snake_case para todos los campos
-    },
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
+    dialectOptions: process.env.DB_SSL === 'true' ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    } : {},
 });
 
 // Función para conectar a la base de datos
